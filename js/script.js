afb0704e8c41d74fcd221a7290f01799fe235209
caroltatto@gmail.com <121826904+caroltatto@users.noreply.github.com>
2023-03-28 13:38:35 -0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
 function carregaDicionario() {
     var biografias = {
         "bio 1": {
-            nome: "Marielle Franco",
-            imagem: "img/marielle-franco.png",
-            descricao: "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros.",
-            citacao: "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?"
+            nome: "Amelia Earhart",
+            imagem: "img/ameliaearhart.jpg",
+            descricao: "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
+            citacao: "As mulheres devem tentar fazes as coisas que os homens tentaram. Quando falhamos, o erro deve ser um desafio para outras."
         },
 
         "bio 2": {
-            nome: "Lélia Gonzales",
-            imagem: "img/lelia-gonzalez.png",
-            descricao: "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira.Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
-            citacao: "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora."
+            nome: "Bertha Von Suttner",
+            imagem: "img/bertha.jpg",
+            descricao: "A austro-húngara Bertha Von Suttner foi a primeira mulher a ganhar o Prêmio Nobel da Paz, em 1905. Inclusive, ela foi uma das responsáveis pela criação do prêmio, pois ajudou o amigo Alfred Nobel na idealização da homenagem, ocorrida pela primeira vez em 1901. Bertha dedicou sua vida à escrita e a pacificação. Seu romance Abaixo às armas, de 1889, se tornou conhecido como um manifesto antimilitarista, pois expõe a violência das guerras sob o ponto de vista de uma mulher.",
+            citacao: "Depois do verbo amar, ajudar é o verbo mais lindo do mundo."
         },
 
         "bio 3": {
-            nome: "Angela Davis",
-            imagem: "img/angela-davis.png",
-            descricao: "Angela Yvonne Davis é uma professora e filósofa socialista estado-unidense que alcançou notoriedade mundial na década de 1970 como integrante do Partido Comunista dos Estados Unidos, dos Panteras Negras, por sua militância pelos direitos das mulheres e contra a discriminação social e racial nos Estados Unidos e por ser personagem de um dos mais polêmicos e famosos julgamentos criminais da recente história dos Estados Unidos.",
-            citacao: "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar."
+            nome: "Leila Diniz",
+            imagem: "img/leiladiniz.jpg",
+            descricao: "Em plena ditadura militar, Leila Diniz foi uma grande defensora da emancipação feminina e do amor livre. Além disso, foi pioneira ao usar bíquini na praia durante sua gravidez, abrindo um caminho para que esse tabu fosse desmistificado. O fato de Leila fazer parte das mulheres que marcaram história não é por acaso. Considerada uma das maiores musas da televisão brasileira, ela era a que mais quebrava o tabu na hora de falar sobre sexo. A brasileira morreu com 27 anos em um acidente aéreo, quando voltava de uma viagem à Austrália",
+            citacao: "Sempre andei sozinha. Me dou bem comigo mesma"
         },
 
         "bio 4": {
-            nome: "Conceição Evaristo",
-            imagem: "img/conceicao-evaristo.png",
-            descricao: "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
-            citacao: "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos."
+            nome: "Malala Yousafzai",
+            imagem: "img/malala.jpg",
+            descricao: "Malala Yousafzai também está na nossa lista de mulheres importantes na história. Se por um lado Marie Curie foi a primeira pessoa a ser contemplada com dois Prêmios Nobel, por outro, a paquistanesa foi a mais jovem a recebê-lo — tendo apenas 17 anos na época. O ativismo de Malala começou cedo: com apenas 11 anos de idade ela já escrevia relatos sobre a ocupação talibã. Sem contar que, aos 15 anos, levou três tiros na cabeça por causa de suas ações ativistas. A boa notícia é que, além de sobreviver, Malala segue defendendo o direito ao estudo.",
+            citacao: "Houve um tempo em que as mulheres pediam aos homens que lutassem pelos direitos delas. Mas agora, nós lutaremos por nós mesmas!"
         },
 
         "bio 5": {
-            nome: "Carolina de Jesus",
-            imagem: "img/carolina-de-jesus.png",
-            descricao: "Carolina Maria de Jesus foi uma escritora brasileira, conhecida por seu livro Quarto de Despejo: Diário de uma Favelada publicado em 1960. Considerada uma das primeiras e mais importantes escritoras negras do Brasil. A autora viveu boa parte de sua vida na favela do Canindé, na zona norte de São Paulo, sustentando a si mesma e seus três filhos como catadora de papéis. Em 1958 tem seu diário publicado sob o nome Quarto de Despejo, com auxílio do jornalista Audálio Dantas. O livro fez um enorme sucesso e chegou a ser traduzido para quatorze línguas.",
-            citacao: "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é."
+            nome: "Maria da Penha",
+            imagem: "img/mariadapenha.jpg",
+            descricao: "A famosa Lei Maria da Penha, que defende mulheres vítimas de violência doméstica existe no Brasil, existe por causa da luta de uma brasileira de mesmo nome. Depois de quase ser assassinada, em duas tentativas de seu ex-marido, Maria da Penha lutou por 20 anos para que o agressor e o estado fossem punidos. Todo seu esforço não foi em vão, e a lei que leva seu nome entrou em vigor em 2006. Para além dessa conquista, ela passou a coordenar uma ONG que auxilia vítimas de violência doméstica, trabalhando no combate deste problema. Essa brasileira com certeza merece estar entre as mulheres históricas!",
+            citacao: "A vida começa quando a violência acaba."
         },
 
         "bio 6": {
-            nome: "Rosa Parks",
-            imagem: "img/rosa-parks.png",
-            descricao: "Rosa Louise McCauley, mais conhecida por Rosa Parks, foi uma costureira negra norte-americana, símbolo do movimento dos direitos civis dos negros nos Estados Unidos. Ficou famosa, em 1º de dezembro de 1955, por ter-se recusado frontalmente a ceder o seu lugar no ônibus a um branco, tornando-se o estopim do movimento que foi denominado boicote aos ônibus de Montgomery e posteriormente viria a marcar o início da luta antissegregacionista.",
-            citacao: "Você ⁠nunca deve ter medo do que está fazendo quando está certo."
+            nome: "Valentina Tereshkova",
+            imagem: "img/valentina.jpg",
+            descricao: "Valentina Tereshkova, a primeira astronauta que foi ao espaço. A russa se destacou entre as demais profissionais por ser uma excelente paraquedista, que era uma habilidade essencial para o voo com a nave Vostok VI. Como resultado de seus feitos como astronauta, ela recebeu duas das maiores condecorações nacionais, o Herói da União Soviética e a Ordem de Lenin. Alguns anos após isso, Valentina ingressou na vida política.",
+            citacao: "Um pássaro não pode voar com uma asa só. O vôo espacial humano não pode se desenvolver mais sem a participação ativa das mulheres."
         },
 
     };
